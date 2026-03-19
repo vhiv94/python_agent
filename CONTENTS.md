299f864dda2263d67e37c7646443782fba448f3a
vhiv94 <vhiv94@gmail.com>
2026-03-19 18:49:57 -0500

    Pass Lesson 4: Token Metadata

--- CONTENTS.md
+++ CONTENTS.md
@@ -1,3 +1,8 @@
 # Table of Contents
 
 1. Ch1: LLMs
+   1. Build an AI Agent
+   2. Python Setup
+   3. Gemini API
+   4. Token Metadata
+2. 
--- main.py
+++ main.py
@@ -17,6 +17,8 @@ model = "gemini-2.5-flash"
 def main():
     prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(model=model, contents=prompt)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
