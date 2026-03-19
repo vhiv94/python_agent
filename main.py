8e0654eefffce133040d07d1565e9dfae65a8a2d
vhiv94 <vhiv94@gmail.com>
2026-03-19 18:35:00 -0500

    Pass Lesson 3: Gemini API

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+
+## gemini api variables
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("please provide a valid gemini api key in .env")
+
+client = genai.Client(api_key=api_key)
+model = "gemini-2.5-flash"
+
+
 def main():
-    print("Hello from ai-agent!")
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model=model, contents=prompt)
+    print(response.text)
 
 
 if __name__ == "__main__":
