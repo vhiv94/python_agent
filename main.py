b7e9b496fbb47bfa2a4df81416b4581945d2098d
vhiv94 <vhiv94@gmail.com>
2026-03-19 19:28:30 -0500

    Pass Lesson 5: User Input

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 
-load_dotenv()
-
-## gemini api variables
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key is None:
-    raise RuntimeError("please provide a valid gemini api key in .env")
-
-client = genai.Client(api_key=api_key)
-model = "gemini-2.5-flash"
-
-
 def main():
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model=model, contents=prompt)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    load_dotenv()
+
+    ## gemini api variables
+    api_key: str = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("please provide a valid gemini api key in .env")
+
+    client = genai.Client(api_key=api_key)
+    model: str = "gemini-2.5-flash"
+
+    ## argparse variables
+    parser = argparse.ArgumentParser(prog="Agentic Dev", description="Toy Agent from Boot.Dev module")
+    # parser.add_help = True
+    parser.add_argument("user_prompt", type=str, help="The prompt to be passed to the LLM")
+    args = parser.parse_args()
+
+    prompt: str = args.user_prompt
+    if prompt is not None:
+        response = client.models.generate_content(model=model, contents=prompt)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
 
 
 if __name__ == "__main__":
