84afcb2e48435fa7df548d5dcde8052dfa33320e
vhiv94 <vhiv94@gmail.com>
2026-03-22 21:58:51 -0500

    Pass Lesson 4: Calling Functions

--- call_function.py
+++ call_function.py
@@ -1,8 +1,40 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.write_file import write_file, schema_write_file
+from functions.run_python_file import run_python_file, schema_run_python_file
+
+
+function_map: dict[str, types.FunctionCall] = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "write_file": write_file,
+    "run_python_file": run_python_file,
+}
+
+def call_function(function_call: types.FunctionCall, verbose: bool = False) -> types.Content:
+    function_name: str = function_call.name or ""
+
+    def get_result(res_or_err: str, response: str) -> types.Content:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={res_or_err: response}
+                )
+            ]
+        )
+    
+    
+    ## call the function
+    print(f"Calling function: {function_call.name}({function_call.args})" if verbose else f" - Calling function: {function_call.name}")
+    if function_name not in function_map:
+        return get_result("error", f"Unkown function: {function_name}")
+    kwargs: dict[str, any] = function_call.args.copy() if function_call.args else {}
+    kwargs["working_directory"] = "./calculator"
+    function_result: str = function_map[function_name](**kwargs)
+    return get_result("result", function_result)
 
 
 available_functions = types.Tool(
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import argparse
 from dotenv import load_dotenv
-from functools import reduce
+# from functools import reduce
 from google.genai import Client, types
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import call_function, available_functions
 
 
 def main():
@@ -38,7 +38,21 @@ def main():
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
         if response.function_calls:
-            print(reduce(lambda acc, call: f"{acc}\nCalling function: {call.name}({call.args})", response.function_calls, ""))
+            # print(reduce(lambda acc, call: f"{acc}\nCalling function: {call.name}({call.args})", response.function_calls, ""))
+            # function_call_results: list[types.Content] = list(map(lambda call: call_function(call, args.verbose), response.function_calls))
+            function_response_list: list[str] = []
+            for function in response.function_calls:
+                result: types.Content = call_function(function, args.verbose)
+                if not result.parts:
+                    raise Exception(f'Error: function call "{function.name}": resulting Content.parts is empty')
+                if not result.parts[0].function_response:
+                    raise Exception(f'Error: function call "{function.name}": resulting Content.parts.function_response is empty')
+                if not result.parts[0].function_response.response:
+                    raise Exception(f'Error: function call "{function.name}": resulting Content.parts.function_response.response is empty')
+                if args.verbose:
+                    print(f'{function.name} -> {result.parts[0].function_response.response}') 
+                function_response_list.append(result.parts[0].function_response.response)
+
         else:
             print(response.text)
 
